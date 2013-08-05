81510e7c09817b8a9c6cfaf6768d7c70c07ff725
Thiago Alessio <thiagoalessio@me.com>
2013-08-05 13:58:50 +0200

    loads rvm on startup

--- environment.zsh
+++ environment.zsh
@@ -1,10 +1,14 @@
 # environment.zsh
 #
 # if tmux is installed, automatically attach or create a new session
+# also loads ruby version manager
 #
 # @package mytoolset
 # @subpackage zsh
 # @author thiagoalessio
+
+PATH=$PATH:$HOME/.rvm/bin
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 if command -v tmux >/dev/null 2>&1; then
 

fd8c909ab86707b6e63333c7915ab59f7eee2f09
Urs Martini <info@urs-martini.de>
2016-08-22 14:58:57 +0200

    made powerline work on OSX as well

--- powerline.zsh
+++ powerline.zsh
@@ -1,8 +1,20 @@
 
-# Add python eggs path to PATH
-PATH=$PATH:$HOME/.local/bin
 
 # powerline
-if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then 
-  source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+if [[ `uname` == 'Darwin' ]]
+then
+    POWERLINE_PATH=~/Library/Python/2.7/lib/python
+
+    # Add python eggs path to PATH
+    PATH=$PATH:$HOME/Library/Python/2.7/bin
+else
+    POWERLINE_PATH=~/.local/lib/python2.7
+
+    # Add python eggs path to PATH
+    PATH=$PATH:$HOME/.local/bin
+fi
+
+if [[ -r $POWERLINE_PATH/site-packages/powerline/bindings/zsh/powerline.zsh ]]; 
+then 
+  source $POWERLINE_PATH/site-packages/powerline/bindings/zsh/powerline.zsh
 fi
